83a5561cad9e07108d5a081bc544fc55c0dfedda
Hong <hongyawei@yit.com>
2018-02-12 10:41:43 +0800

    优化

--- src/js/app/modules/chat/initGetGreetings.js
+++ src/js/app/modules/chat/initGetGreetings.js
@@ -32,10 +32,15 @@ function _getGreetings(officialAccount){
 		// vip用户提示
 		function insertVipTip() {
 			var chatWrapper = document.querySelector(".chat-container");
-			var dateList = chatWrapper.querySelectorAll(".em-widget-date");
-			var domToInsert = dateList[dateList.length - 1]
-			var vipTip = '<div class="vip-tip"><p>尊敬的VIP会员，您的客服请求将得到优先响应</p></div>';
-			domToInsert.innerHTML = domToInsert.innerHTML + vipTip;
+			var dateList = []
+			if (chatWrapper != null) {
+				var dateList = chatWrapper.querySelectorAll(".em-widget-date");
+				if (dateList.length > 0) {
+					var domToInsert = dateList[dateList.length - 1]
+					var vipTip = '<div class="vip-tip"><p>尊敬的VIP会员，您的客服请求将得到优先响应</p></div>';
+					domToInsert.innerHTML = domToInsert.innerHTML + vipTip;
+				}
+			}
 		}
 
 		if (localStorage.yitiao_vipState) {
